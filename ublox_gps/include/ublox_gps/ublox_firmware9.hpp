577ef65095bdc5f2cd1a2ea32a4dbf13dbcd0b7c
brandonbeggs <52388984+brandonbeggs@users.noreply.github.com>
2023-11-05 11:13:33 -0600

    Added Firmware v9 GNSS configuration (#229)
    * Added Firmware v9 GNSS configuration
    
    * Update CfgVALSETCfgdata.msg
    
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

--- ublox_gps/include/ublox_gps/ublox_firmware9.hpp
+++ ublox_gps/include/ublox_gps/ublox_firmware9.hpp
@@ -7,6 +7,9 @@
 #include <diagnostic_updater/diagnostic_updater.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <ublox_msgs/msg/cfg_valset.hpp>
+#include <ublox_msgs/msg/cfg_valset_cfgdata.hpp>
+
 #include <ublox_gps/fix_diagnostic.hpp>
 #include <ublox_gps/gnss.hpp>
 #include <ublox_gps/ublox_firmware8.hpp>
@@ -15,12 +18,29 @@ namespace ublox_node {
 
 /**
  *  @brief Implements functions for firmware version 9.
- *  For now it simply re-uses the firmware version 8 class
- *  but allows for future expansion of functionality
  */
 class UbloxFirmware9 final : public UbloxFirmware8 {
 public:
   explicit UbloxFirmware9(const std::string & frame_id, std::shared_ptr<diagnostic_updater::Updater> updater, std::shared_ptr<FixDiagnostic> freq_diag, std::shared_ptr<Gnss> gnss, rclcpp::Node* node);
+
+  /**
+    * @brief Configure settings specific to firmware 9 based on ROS parameters.
+    *
+    * @details Configure GNSS.  The hardware has internal logic for
+    * detecting differences between the new and active GNSS
+    * configuration and will internally trigger a reset if necessary.
+    * Configure the NMEA if desired by the user. It also may clear the
+    * flash memory based on the ROS parameters.
+    */
+  bool configureUblox(std::shared_ptr<ublox_gps::Gps> gps) override;
+
+private:
+  /**
+    * @brief Populate the CfgVALSETCfgData data type
+    *
+    * @details A helper function used to generate a configuration for a single signal. 
+    */
+  ublox_msgs::msg::CfgVALSETCfgdata generateSignalConfig(uint32_t signalID, bool enable);
 };
 
 }  // namespace ublox_node
--- ublox_gps/src/ublox_firmware9.cpp
+++ ublox_gps/src/ublox_firmware9.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <string>
+#include <vector>
 
 #include <diagnostic_updater/diagnostic_updater.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -15,5 +16,107 @@ UbloxFirmware9::UbloxFirmware9(const std::string & frame_id, std::shared_ptr<dia
   : UbloxFirmware8(frame_id, updater, freq_diag, gnss, node)
 {
 }
+
+bool UbloxFirmware9::configureUblox(std::shared_ptr<ublox_gps::Gps> gps)
+{
+  if (clear_bbr_)
+  {
+    // clear flash memory
+    if (!gps->clearBbr())
+    {
+      RCLCPP_ERROR(node_->get_logger(), "u-blox failed to clear flash memory");
+    }
+  }
+
+  gps->setSaveOnShutdown(save_on_shutdown_);
+
+  //
+  // Configure the GNSS, only if the configuration is different
+  //
+  // First, get the current GNSS configuration
+  ublox_msgs::msg::CfgGNSS cfg_gnss;
+  if (gps->poll(cfg_gnss))
+  {
+    RCLCPP_DEBUG(node_->get_logger(), "Read GNSS config.");
+    RCLCPP_DEBUG(node_->get_logger(), "Num. tracking channels in hardware: %i", cfg_gnss.num_trk_ch_hw);
+    RCLCPP_DEBUG(node_->get_logger(), "Num. tracking channels to use: %i", cfg_gnss.num_trk_ch_use);
+  }
+  else
+  {
+    throw std::runtime_error("Failed to read the GNSS config.");
+  }
+
+  ublox_msgs::msg::CfgVALSET cfg_signal;
+  cfg_signal.layers = ublox_msgs::msg::CfgVALSET::LAYER_RAM;
+
+  using signal = ublox_msgs::msg::CfgVALSETCfgdata;
+
+  // Configure GPS Signals
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GPS_ENABLE, enable_gps_));
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GPS_L1CA_ENABLE, enable_gps_));
+  if (gnss_->isSupported("GPS_L2C"))
+  {
+    cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GPS_L2C_ENABLE, enable_gps_));
+  }
+
+  // Configure SBAS Signals
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::SBAS_ENABLE, enable_gps_));
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::SBAS_L1CA_ENABLE, enable_gps_));
+
+  // Configure Galileo Signals
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GAL_ENABLE, enable_galileo_));
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GAL_E1_ENABLE, enable_galileo_));
+  if (gnss_->isSupported("GAL_E5B"))
+  {
+    cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GAL_E5B_ENABLE, enable_galileo_));
+  }
+
+  // Configure Beidou Signals
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::BDS_ENABLE, enable_beidou_));
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::BDS_B1_ENABLE, enable_beidou_));
+  if (gnss_->isSupported("BDS_B2"))
+  {
+    cfg_signal.cfgdata.push_back(generateSignalConfig(signal::BDS_B2_ENABLE, enable_beidou_));
+  }
+
+  // Configure QZSS Signals
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::QZSS_ENABLE, enable_qzss_ && enable_gps_));
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::QZSS_L1CA_ENABLE, enable_qzss_ && enable_gps_));
+  if (gnss_->isSupported("QZSS_L2C"))
+  {
+    cfg_signal.cfgdata.push_back(generateSignalConfig(signal::QZSS_L2C_ENABLE, enable_qzss_ && enable_gps_));
+  }
+
+  // Configure GLONASS Signals
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GLO_ENABLE, enable_glonass_));
+  cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GLO_L1_ENABLE, enable_glonass_));
+  if (gnss_->isSupported("GLO_L2"))
+  {
+    cfg_signal.cfgdata.push_back(generateSignalConfig(signal::GLO_L2_ENABLE, enable_glonass_));
+  }
+
+  RCLCPP_DEBUG(node_->get_logger(), "Ready to configure");
+  gps->configure(cfg_signal, false);
+
+  //
+  // NMEA config
+  //
+  if (getRosBoolean(node_, "nmea.set") && !gps->configure(cfg_nmea_))
+  {
+    throw std::runtime_error("Failed to configure NMEA");
+  }
+
+  return true;
+}
+
+ublox_msgs::msg::CfgVALSETCfgdata UbloxFirmware9::generateSignalConfig(uint32_t signalID, bool enable)
+{
+  ublox_msgs::msg::CfgVALSETCfgdata signalConfig;
+  signalConfig.key = signalID;
+  signalConfig.data.resize(1);
+  signalConfig.data[0] = enable;
+  return signalConfig;
+}
+
 
 }  // namespace ublox_node
